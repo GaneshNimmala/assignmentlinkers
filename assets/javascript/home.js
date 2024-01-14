ba5a5282d895aecc86f7585010a73342151cdf29
Ganesh <ganeshnimmala65@gmail.com>
2024-01-14 11:55:53 +0530

    Home page

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -23,3 +23,57 @@ function scrollCards(direction) {
   var cardContainer = document.getElementById("cardCarousel");
   cardContainer.scrollLeft += direction * 200; // Adjust the scroll distance as needed
 }
+$(document).ready(function () {
+  // Hide all content divs initially
+  $('[id^="button"]').hide();
+
+  // Button click event
+  $(".half-oven-btn").click(function () {
+    // Remove 'active' class from all buttons
+    $(".half-oven-btn").removeClass("active");
+
+    // Add 'active' class to the clicked button
+    $(this).addClass("active");
+
+    // Hide all content divs
+    $('[id^="button"]').hide();
+
+    // Get the target content div based on the button's data-target attribute
+    var targetContent = $("#" + $(this).data("target") + "-content");
+
+    // Show the target content div
+    targetContent.show();
+  });
+});
+
+///////////////////////////////////////
+// document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+//   anchor.addEventListener("click", function (e) {
+//     e.preventDefault();
+
+//     document.querySelector(this.getAttribute("href")).scrollIntoView({
+//       behavior: "smooth",
+//       block: "start",
+//     });
+//   });
+// });
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href").substring(1); // remove the '#' from the href
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      const headerHeight = 120; // Add the height of your fixed header
+
+      const offsetTop = targetElement.offsetTop - headerHeight;
+
+      window.scrollTo({
+        top: offsetTop,
+        behavior: "smooth",
+      });
+    }
+  });
+});
